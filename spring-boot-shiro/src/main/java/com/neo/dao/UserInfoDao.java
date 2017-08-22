710ec9ad0c29de0d4fa62708c46e1ccb0ea30b25
yaohui 00355492 <yaohui4@huawei.com>
2017-08-22 15:50:45 +0800

    重新设计权限-part1

--- spring-boot-shiro/src/main/java/com/neo/dao/UserInfoDao.java
+++ spring-boot-shiro/src/main/java/com/neo/dao/UserInfoDao.java
@@ -1,9 +1,10 @@
 package com.neo.dao;
 
 import com.neo.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserInfoDao extends CrudRepository<UserInfo,Long> {
+public interface UserInfoDao extends JpaRepository<UserInfo,Long> {
     /**通过username查找用户信息;*/
     public UserInfo findByUsername(String username);
 }
