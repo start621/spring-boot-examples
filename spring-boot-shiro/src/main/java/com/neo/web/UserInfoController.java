7035993b21559941116c8e306b13e028f817fb1d
start621 <yh109096236@126.com>
2017-08-25 23:46:31 +0800

    增加swagger，修改banner

--- spring-boot-shiro/src/main/java/com/neo/web/UserInfoController.java
+++ spring-boot-shiro/src/main/java/com/neo/web/UserInfoController.java
@@ -2,6 +2,7 @@ package com.neo.web;
 
 import com.neo.entity.UserInfo;
 import com.neo.sevice.UserInfoService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ public class UserInfoController {
      * 用户查询.
      * @return all user_info
      */
+    @ApiOperation(value = "用户查询")
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     //@RequiresPermissions("userInfo:view")//权限管理;
     public List<UserInfo> getAllUsers(){
@@ -32,6 +34,7 @@ public class UserInfoController {
      * @param userInfoList 输入用户信息
      * @return 新建用户信息
      */
+    @ApiOperation(value = "批量创建用户接口")
     @RequestMapping(value = "/users", method = RequestMethod.POST)
     public List<UserInfo> createUsers(@RequestBody List<UserInfo> userInfoList) {
         return userService.BatchCreate(userInfoList);
@@ -40,14 +43,17 @@ public class UserInfoController {
     /**
      * 批量删除用户
      */
+    @ApiOperation(value = "批量删除用户")
     @RequestMapping(value = "/users", method = RequestMethod.DELETE)
     public void deleteUsers(){
         userService.batchDelete();
     }
+
     /**
      * 用户添加;
      * @return 返回新建用户名
      */
+    @ApiOperation(value = "用户添加")
     @RequestMapping(value = "/users/user", method = RequestMethod.POST)
     //@RequiresPermissions("userInfo:add")//权限管理;
     public String userInfoAdd(@RequestBody UserInfo userInfo){
@@ -55,12 +61,14 @@ public class UserInfoController {
         return userService.create(userInfo).getUsername();
     }
 
+    @ApiOperation(value = "用户信息修改")
     @RequestMapping(value = "/users/user", method = RequestMethod.PUT)
     //@RequiresPermissions("userInfo:add")//权限管理;
     public String userInfoUpdate(@RequestBody UserInfo userInfo){
         return userService.update(userInfo).getUsername();
     }
 
+    @ApiOperation(value = "根据用户名查找")
     @RequestMapping(value = "/users/{username}", method = RequestMethod.GET)
     public UserInfo getUserInfo(@PathVariable String username) {
         return  userService.findByUsername(username);
@@ -69,6 +77,7 @@ public class UserInfoController {
      * 用户删除;
      * @return message
      */
+    @ApiOperation(value = "根据用户Id删除")
     @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
     //@RequiresPermissions("userInfo:del")//权限管理;
     public String  userInfoDel(@PathVariable String id){
